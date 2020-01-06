ffbf4cb433f0ce9a544afc69cb76311975e5c18a
陈海洋 <1559843332@qq.com>
2020-01-06 17:49:21 +0800

    添加todolist-添加删除功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        count: 1
-    },
-    mutations: {
-        increase() {
-            this.state.count++
-        }
-    },
-    actions: {},
-    modules: {}
+  state: {
+    count: 1
+  },
+  mutations: {
+    increase () {
+      this.state.count++
+    }
+  },
+  actions: {},
+  modules: {}
 })
