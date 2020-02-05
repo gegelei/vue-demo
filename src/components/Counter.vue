c552463e3010c81166a78b32a83419c1bcb03f17
陈海洋 <1559843332@qq.com>
2020-02-05 12:09:33 +0800

    格式化代码 his.$data、this._data、this.xxx 都能获取数据

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,42 +1,42 @@
 <template>
-<!--    <div @click="add">{{number}}</div>-->
+    <!--    <div @click="add">{{number}}</div>-->
     <div @click="add1">{{NewContent}}</div>
 </template>
 
 <script>
   export default {
-      //组件参数校验
-      props: {
-          //方法一，以数组形式，下例表示字符串和数组都可以
-          // content: [String, Number],
-          //方法二，以对象格式
-          content: {
-              type: [String, Number],   //类型
-              required: false,  //是否必传
-              //借助验证器
-              // validator: function (val) {
-              //     return (val.length < 3)
-              // }
-          }
-      },
-      data: function() {
-          return {
-              step: 1,
-              number: 0,
-              NewContent: this.content,
-          }
-      },
+    //组件参数校验
+    props: {
+      //方法一，以数组形式，下例表示字符串和数组都可以
+      // content: [String, Number],
+      //方法二，以对象格式
+      content: {
+        type: [String, Number],   //类型
+        required: false,  //是否必传
+        //借助验证器
+        // validator: function (val) {
+        //     return (val.length < 3)
+        // }
+      }
+    },
+    data: function () {
+      return {
+        step: 1,
+        number: 0,
+        NewContent: this.content,
+      }
+    },
     name: 'Counter',
     methods: {
       add () {
-          this.$data.number++;
-          this.$emit('change');
+        this.$data.number++
+        this.$emit('change')
       },
-        add1 () {
-            //直接操作父组件传的content会报错，在子组件中复制一份即可操作
-            this.$data.NewContent += this.$data.step;
-            this.$emit('change1', this.$data.step);
-        },
+      add1 () {
+        //直接操作父组件传的content会报错，在子组件中复制一份即可操作
+        this.NewContent += this.step
+        this.$emit('change1', this.step)
+      },
     }
   }
 </script>
--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <button type="button" @click="add()">添加</button>
+        <button type="button" @click="add()">添加(vuex)</button>
+        <hr/>
+
+
 
         <div><h1>TodoList</h1></div>
         <input v-model="inputValue"/>
-        <div>
-        <button @click="buttonclick" v-if="show">测试{{text}}</button>
-            <div v-for="(item,index) of Newlist">{{item.name}}--{{index}}</div>
-        </div>
         <button @click="tianjia">添加</button>
         <div>
             <!--组件多种写法-->
@@ -18,32 +17,48 @@
             <div is="todo-item" v-for="(item,index) in list" :key="item" :content="item" :index="index"
                  @zidel="delMethod"></div>
         </div>
-        ----------------------------------
+        <hr/>
+
+
+
+        <div>
+            <button @click="buttonclick" v-if="show">测试{{text}}</button>
+            <div v-for="(item,index) of Newlist">{{item.name}}--{{index}}</div>
+        </div>
+        <hr/>
+
+
+
         <!--父子组件数据传递demo1,使用ref完成计数器-->
-<!--        <div>-->
-<!--            <counter @change="makeCount" ref="count1"></counter>-->
-<!--            <counter @change="makeCount" ref="count2"></counter>-->
-<!--            <div>{{total}}</div>-->
-<!--        </div>-->
+        <!--        <div>-->
+        <!--            <counter @change="makeCount" ref="count1"></counter>-->
+        <!--            <counter @change="makeCount" ref="count2"></counter>-->
+        <!--            <div>{{total}}</div>-->
+        <!--        </div>-->
         <!--父子组件数据传递demo2,单向数据流测试和第二种方法实现计数器-->
         <div>
             <counter :content="1" @change1="handleclick"></counter>
             <counter :content="1" @change1="handleclick"></counter>
             <div>{{total1}}</div>
         </div>
-        -------------------------------------------------------
+        <hr/>
+
+
+
         <!--   插槽slot   study  -->
         <div>
-            <!--直接调用父组件的原生事件-->
+            <!--直接调用父组件的原生事件666-->
             <slotTest @click.native="handletest">
-            <!--使用插槽的方法向子组件传递大量数据，保证代码整洁性-->
-            <!--    加上slot属性后，子组件中可分开使用插槽   -->
-                <div slot="header"> header </div>
-                <div slot="footer"> footer </div>
+                <!--使用插槽的方法向子组件传递大量数据，保证代码整洁性-->
+                <!--    加上slot属性后，子组件中可分开使用插槽   -->
+                <div slot="header"> header</div>
+                <div slot="footer"> footer</div>
             </slotTest>
-
         </div>
-        ------------------------------------------------------------
+        <hr/>
+
+
+
         <!--   非父子组件传值 bus/观察者模式 实现-->
         <div>
             <busTest content="sea"></busTest>
@@ -58,59 +73,59 @@
   import Counter from '../components/Counter'
   import SlotTest from '../components/SlotTest'
   import BusTest from '../components/BusTest'
-  import Vue from "vue";
+  import Vue from 'vue'
   //将bus属性挂载到所有的vue实例中
-  Vue.prototype.bus = new Vue();
+  Vue.prototype.bus = new Vue()
 
   export default {
     name: 'info',
     store,
     components: {
-        'todo-item': TodoItem,
-        'counter': Counter,
-        'slotTest': SlotTest,
-        'busTest': BusTest,
+      'todo-item': TodoItem,
+      'counter': Counter,
+      'slotTest': SlotTest,
+      'busTest': BusTest,
     },
     data () {
       return {
         inputValue: '',
-          list: [],
-          Newlist: [{name:'dalei'},{name:'sea'},{name:'dong'}],
-          text: '',
-          text1: 1,
-          text2: 2,
-          show: true,
-          total: 0,
-          total1: 2,
+        list: [],
+        Newlist: [{ name: 'dalei' }, { name: 'sea' }, { name: 'dong' }],
+        text: '',
+        text1: 1,
+        text2: 2,
+        show: true,
+        total: 0,
+        total1: 2,
       }
     },
 
     methods: {
-        buttonclick() {
-            this.text = this.text1 + this.text2;
-            // this.show = !this.show;
-            this.Newlist.sort();
-        },
+      buttonclick () {
+        this.text = this.text1 + this.text2
+        // this.show = !this.show;
+        this.Newlist.sort()
+      },
       add () {
         store.commit('increase')
         console.log('h')
       },
       tianjia () {
         this.list.push(this.inputValue)
-          console.log(this.list);
+        console.log(this.list)
         this.inputValue = ''
       },
       delMethod (index) {
         this.list.splice(index, 1)
       },
-        makeCount () {
-            this.total = this.$refs.count1.number + this.$refs.count2.number;
-        },
-        handleclick (value) {
-            this.total1 += value;
-        },
-        handletest () {
-        },
+      makeCount () {
+        this.total = this.$refs.count1.number + this.$refs.count2.number
+      },
+      handleclick (value) {
+        this.total1 += value
+      },
+      handletest () {
+      },
     }
   }
 
